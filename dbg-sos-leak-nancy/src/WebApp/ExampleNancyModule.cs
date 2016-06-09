185015a724bf38fb43da12ebe0ce42b1a78a2889
shchegrikovich <shchegrikovich@gmail.com>
2016-06-09 23:43:35 +0300

    Added leak demo

---
+++ dbg-sos-leak-nancy/src/WebApp/ExampleNancyModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+
+namespace NancyMemoryLeak
+{
+    public class ExampleNancyModule : NancyModule
+    {
+        public ExampleNancyModule()
+        {
+            Get["/v1/feeds"] = parameters =>
+            {
+                var feeds = new string[] { "foo", "bar" };
+                return Response.AsJson(feeds);
+            };
+        }
+    }
+}
---
+++ dbg-sos-leak-nancy/src/WebApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using Nancy;
+using Nancy.Hosting.Self;
+
+namespace NancyMemoryLeak
+{
+    static class Program
+    {
+        private static NancyHost _nancyHost;
+        private static readonly CancellationTokenSource Source = new CancellationTokenSource();
+
+        static void Main(string[] args)
+        {
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                eventArgs.Cancel = true;
+                Source.Cancel();
+            };
+
+            _nancyHost = new NancyHost(new Uri("http://localhost:5000"), new DefaultNancyBootstrapper(), new HostConfiguration
+            {
+                UrlReservations = new UrlReservations
+                {
+                    CreateAutomatically = true
+                }
+            });
+            _nancyHost.Start();
+
+
+            Console.WriteLine("Press Ctrl-C to exit.");
+            WaitHandle.WaitAll(new[] { Source.Token.WaitHandle });
+        }
+    }
+}
